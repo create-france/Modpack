18aa370733a78a4ff88ebd83d931ac992c304085
Antonin LYAËT <31038778+syukoGit@users.noreply.github.com>
2024-02-24 22:02:07 +0100

    Update REIScripts.js

--- overrides/kubejs/client_scripts/REIScripts.js
+++ overrides/kubejs/client_scripts/REIScripts.js
@@ -1,5 +1,7 @@
 REIEvents.hide('item', event => {
     event.hide('botania:terraform_rod');
     event.hide('botania:spawner_mover');
+    event.hide('botania:mana_gun');
+    
     event.hide('clutter:copper_nugget');
 });
